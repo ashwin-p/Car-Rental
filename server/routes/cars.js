67546ce4206cc158c834ca8b7e3527e62ff4fedc
Ashwin <ashwin.ponnur@gmail.com>
2023-11-17 16:12:16 +0530

    fixed cars.js

--- server/routes/cars.js
+++ server/routes/cars.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const Car = require('../models/Car');
 
+// Get all cars
 router.get('/', async (req, res) => {
   try {
     const cars = await Car.find();
@@ -11,6 +12,20 @@ router.get('/', async (req, res) => {
   }
 });
 
+// Get a specific car by ID
+router.get('/:id', async (req, res) => {
+  try {
+    const car = await Car.findById(req.params.id);
+    if (!car) {
+      return res.status(404).json({ message: 'Car not found' });
+    }
+    res.json(car);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+// Create a new car
 router.post('/', async (req, res) => {
   const car = new Car({
     make: req.body.make,
@@ -26,4 +41,44 @@ router.post('/', async (req, res) => {
   }
 });
 
+// Update a car by ID
+router.put('/:id', async (req, res) => {
+  try {
+    const car = await Car.findByIdAndUpdate(
+      req.params.id,
+      {
+        make: req.body.make,
+        model: req.body.model,
+        year: req.body.year,
+      },
+      { new: true }
+    );
+
+    if (!car) {
+      return res.status(404).json({ message: 'Car not found' });
+    }
+
+    res.json(car);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+// Delete a car by ID
+router.delete('/:id', async (req, res) => {
+  try {
+    const car = await Car.findByIdAndRemove(req.params.id);
+
+    if (!car) {
+      return res.status(404).json({ message: 'Car not found' });
+    }
+
+    res.json({ message: 'Car deleted successfully' });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
 module.exports = router;
+
+
